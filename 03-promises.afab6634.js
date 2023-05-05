7b0d1db0e2ccc7d5bad35abc40a07daad554ace2
nezha444 <87755452+nezha444@users.noreply.github.com>
2023-05-05 11:08:11 +0000

    Deploying to gh-pages from @ nezha444/goit-js-hw-08@453dcd154b6679b1b1c9935d5d4703e5130eab97 🚀

---
+++ 03-promises.afab6634.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector('[type="submit"]');document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]');e.addEventListener("submit",t);var t=setInterval((function(e,t){Math.random()}),1e3)}();
+//# sourceMappingURL=03-promises.afab6634.js.map
---
+++ 03-promises.bf94e71e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('[type="submit"]');document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]');e.addEventListener("submit",t);const t=setInterval((function(e,t){Math.random()}),1e3);
+//# sourceMappingURL=03-promises.bf94e71e.js.map
